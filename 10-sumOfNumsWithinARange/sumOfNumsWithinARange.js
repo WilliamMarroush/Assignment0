3da2488fb1d40f5bb3f4cda4836e8ff9d9c3c585
WilliamMarroush <marroushw@yahoo.com>
2021-12-30 14:56:12 -0500

    Final commit of assignment0-10

--- 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
+++ 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -1,5 +1,9 @@
 function sumOfNumsWithinARange(nums, start, end) {
-  // Insert code here;
+	let count=0;
+	for (let steps=0;steps<nums.length;steps++){
+		if (nums[steps]>=start && nums[steps]<=end){
+			count++;}}
+	return count;
 }
 
 // Do not edit this line;
