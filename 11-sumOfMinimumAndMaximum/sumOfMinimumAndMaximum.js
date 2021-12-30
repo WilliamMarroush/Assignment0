fe3cd6da070cb515c0f0db39c1aaf5fd97e2b1c7
WilliamMarroush <marroushw@yahoo.com>
2021-12-30 15:03:29 -0500

    Final commit of assignment0-11

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,5 +1,16 @@
 function sumOfMinimumAndMaximum(nums) {
-  // Insert code here;
+	let finalsum=0;
+	let max=0;
+	var min;
+	for (let steps=0;steps<nums.length;steps++){
+		if (nums[steps]>max){
+			max = nums[steps];}}
+	min = nums[0];
+	for (let count=0;count<nums.length;count++){
+		if (nums[count]<min){
+			min=nums[count];}}
+	finalsum = min + max;
+	return finalsum;
 }
 
 // Do not edit this line;
