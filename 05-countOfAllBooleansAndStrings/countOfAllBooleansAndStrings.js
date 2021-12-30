dda5a7a319a2253d5f533a44792ab30b8245ce57
WilliamMarroush <marroushw@yahoo.com>
2021-12-30 14:20:29 -0500

    Final commit for assignment0-05

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,5 +1,15 @@
 function countOfAllBooleansAndStrings(arr) {
-  // Insert code here;
+	let count =0;
+	for (let step=0;step<arr.length;step++){
+		if (typeof(arr[step])=="boolean"){
+			count++;}
+	}
+	 for (let tep=0;tep<arr.length;tep++){
+                if (typeof(arr[tep])=="string"){
+                        count++;}
+        }
+
+	return count;
 }
 
 // Do not edit this line;
