68d09421d220455fd58f0077c2cc985fa497e589
WilliamMarroush <marroushw@yahoo.com>
2021-12-30 13:47:39 -0500

    Final commit for assignment0-01

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,14 @@
 function titleCaseEdit(title) {
-  // Insert code here;
+  	text = title.split(" ");
+	finaltext = "";
+	for (let step = 0;step<text.length;step++){
+		addtext = text[step].charAt(0).toUpperCase()+text[step].slice(1);
+		if (step<text.length-1){
+			finaltext+=addtext+" ";}
+		else
+			finaltext+=addtext;
+	}
+	return finaltext;
 }
 
 // Do not edit this line;
