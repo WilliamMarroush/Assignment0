633721ad3d2f75499320414f209542b3067b95ce
WilliamMarroush <marroushw@yahoo.com>
2021-12-30 16:09:39 -0500

    Final commit for assignment0-14

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,13 @@
 function frequencyCounter(word) {
-  // Insert code here;
+	var frequency = {};
+	for (let steps=0;steps<word.length;steps++){
+		let character = word.charAt(steps);
+		if (frequency[character]){
+			frequency[character]++;}
+		else{
+			frequency[character]=1;}
+	}
+	return frequency;
 }
 
 // Do not edit this line;
