34ce4259f651c4ced5415707b0de5f26a1aba3b7
WilliamMarroush <marroushw@yahoo.com>
2021-12-30 16:48:20 -0500

    Final commit of assignment0-19

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -4,10 +4,23 @@ class MySolution {
   }
 
   binarySearch(nums, target) {
-    // Insert code here;
+	  let middle = Math.floor(nums.length/2);
+	  let smallerarray = nums.slice(0);
+	  if (smallerarray[middle]==target){
+		  return true;}
+	  if (smallerarray[middle]<target){
+		  smallerarray = smallerarray.splice(middle,smallerarray.length-1);
+		  return (this.binarySearch(smallerarray,target));
+	  }
+	  else if (smallerarray[middle]>target){
+		  smallerarray=smallerarray.splice(0,middle);
+		  return (this.binarySearch(smallerarray,target));}
+	  else
+		  return false;
   }
 }
 
 // Do not edit this line;
 let StudentSolution = MySolution;
 module.exports = StudentSolution;
+
