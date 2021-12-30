5fbfed5c081068f7aec248b327730fc3dd25dbd6
WilliamMarroush <marroushw@yahoo.com>
2021-12-30 15:10:34 -0500

    Final commit for assignment0-12

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,5 +1,16 @@
 function fizzBuzz(start, end) {
-  // Insert code here;
+	const fizzbuzz=[];
+	for (let steps=start;steps<=end;steps++){
+		if (steps%3==0 && steps%5==0){
+			fizzbuzz.push("FizzBuzz");}
+		else if (steps%3==0){
+			fizzbuzz.push("Fizz");}
+		else if (steps%5==0){
+			fizzbuzz.push("Buzz");}
+		else
+			fizzbuzz.push(steps);
+	}
+	return fizzbuzz;
 }
 
 // Do not edit this line;
