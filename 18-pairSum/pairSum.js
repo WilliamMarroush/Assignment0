b50c402237a90a803d2951c0890051082e9d2050
WilliamMarroush <marroushw@yahoo.com>
2021-12-30 16:30:21 -0500

    Final commit for assignment0-18

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,13 @@
 function pairSum(nums, target) {
-  // Insert code here;
+	if (nums.length<=1){
+		throw 'Not enough values in array. Default false.';}
+	let doesitadd = false;
+	for (let steps=0;steps<nums.length;steps++){
+		for (let pair=0;pair<nums.length;pair++){
+			if (nums[steps]+nums[pair]==target){
+				return true;}}}
+	return doesitadd;
+
 }
 
 // Do not edit this line;
