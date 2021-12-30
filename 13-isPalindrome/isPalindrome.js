134a3243449f01a3da14cb663497a2a49bc82d67
WilliamMarroush <marroushw@yahoo.com>
2021-12-30 15:45:09 -0500

    Final commit for assignment0-13

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,5 +1,17 @@
 function isPalindrome(word) {
-  // Insert code here;
+	const backwards = [];
+	let track = 0;
+	for (let steps = word.length-1;steps>=0;steps--){
+		backwards.push(word.charAt(steps));
+	}
+	for (let count = 0;count<word.length;count++){
+		if (backwards[count]==word.charAt(count)){
+			track++;}}
+	if (track == word.length){
+		return true;}
+	else 
+		return false;
+	
 }
 
 // Do not edit this line;
