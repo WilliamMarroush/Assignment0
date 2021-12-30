b77c9d0b468f85df90a777ab18cc902cc512f86b
WilliamMarroush <marroushw@yahoo.com>
2021-12-30 14:52:59 -0500

    Final commit for assignment0-09

--- 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
+++ 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
@@ -1,6 +1,11 @@
 function sumOfAllOddNumbers(nums) {
-  // Insert code here;
+	let count =0;
+	for (let steps=0;steps<nums.length;steps++){
+		if ((nums[steps]%2)!=0){
+			count++;}}
+	return count;
 }
 
 // Do not edit this line;
 module.exports = sumOfAllOddNumbers;
+
