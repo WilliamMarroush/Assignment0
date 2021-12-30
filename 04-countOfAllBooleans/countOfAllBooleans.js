ea7318494f201479364715b9993a0a663bcbdd87
WilliamMarroush <marroushw@yahoo.com>
2021-12-30 14:01:47 -0500

    Final commit for assignment0-04

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,5 +1,11 @@
 function countOfAllBooleans(arr) {
-  // Insert code here;
+	let count =0;
+	for (let step=0;step<arr.length;step++){
+		if (typeof(arr[step])=="boolean"){
+			count++;}
+	}
+	return count;
+
 }
 
 // Do not edit this line;
