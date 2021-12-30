3414c35c0e9d4812b2d776af5cd908c81a034633
WilliamMarroush <marroushw@yahoo.com>
2021-12-30 14:45:33 -0500

    Final commit of assignment0-07

--- 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
+++ 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
@@ -1,5 +1,9 @@
 function countOfAllIndexMatchingNumbers(nums) {
-  // Insert code here;
+	let count =0;
+	for (let steps =0;steps<nums.length;steps++){
+		if (nums[steps]==steps){
+			count++;}}
+	return count;
 }
 
 // Do not edit this line;
